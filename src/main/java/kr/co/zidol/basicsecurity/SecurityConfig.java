06c9ca332d2b013509772bcf01d36941c2d98963
zidol <ajh3166@naver.com>
2021-12-06 22:47:19 +0900

    SessionManagementFilter, ConcurrentSessionFilter

--- src/main/java/kr/co/zidol/basicsecurity/SecurityConfig.java
+++ src/main/java/kr/co/zidol/basicsecurity/SecurityConfig.java
@@ -17,10 +17,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated();
         http
-                .formLogin();
+                .formLogin()
+                .and()
+                .sessionManagement()
+                .maximumSessions(1)//최대 허용 세션 수 : 1
+                .maxSessionsPreventsLogin(false) //세션 초과시 현재 로그인 시도 인증 실패
+        ;
 
-
-        http
+//세션 정책
+/*        http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
 //                SessionCreationPolicy. Always 		:  스프링 시큐리티가 항상 세션 생성
@@ -28,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                SessionCreationPolicy. Never   		:  스프링 시큐리티가 생성하지 않지만 이미 존재하면 사용
 //                SessionCreationPolicy. Stateless	 	:  스프링 시큐리티가 생성하지 않고 존재해도 사용하지 않음
                 .sessionFixation().none();   //changeSessionId : 기본값 Servlet 3.1이상 기본값  .. none , migrateSession: 3.1미만 기봇값, newSession
+                */
 
     }
 }
